81d307d7709c84981bd18405208a8eac25b58402
Erin Wise <erinwise88@gmail.com>
2024-02-06 17:18:42 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -20,19 +20,26 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return make_response(bakeries, 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id = id).first()
+    return bakery.to_dict()
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    bg_by_price_serialized = [bg.to_dict() for bg in baked_goods_by_price]
+
+    return make_response(bg_by_price_serialized, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_bg = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    bg_serialized = most_expensive_bg.to_dict()
+    return make_response(bg_serialized, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
